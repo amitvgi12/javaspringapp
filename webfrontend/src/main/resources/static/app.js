108692de3ade965270751a977e55818e0f54eb0f
amitvgi12 <52849460+amitvgi12@users.noreply.github.com>
2020-04-07 14:22:40 +0530

    Create app.js

---
+++ webfrontend/src/main/resources/static/app.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+var app = angular.module('myApp', ['ngRoute']);
+
+app.controller('MainController', function($scope, $http) {
+
+    $scope.messages = [];
+    $scope.sayHelloToServer = function() {
+        $http.get("/greeting?_=" + Date.now()).then(function(response) {
+            $scope.messages.push(response.data);
+        });
+    };
+    
+    $scope.sayHelloToServer();
+});
+© 2020 GitHub, Inc.
